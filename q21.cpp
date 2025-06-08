092beb191a73b3616c75de001316f8ed3d8a730b
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2025-06-07 21:51:44 -0400

    some more

--- q21.cpp
+++ q21.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <optional>
+#include <unordered_set>
 
 const std::vector<std::string> num_keypad = {
     "789",
@@ -16,6 +17,18 @@ const std::vector<std::string> dir_keypad = {
     " ^A",
     "<v>"};
 constexpr std::pair<size_t, size_t> dir_start = std::make_pair(0, 2);
+
+std::optional<std::pair<size_t, size_t>> get_pos(const std::vector<std::string> &keypad, char c) {
+    for (size_t i = 0; i < keypad.size(); ++i) {
+        for (size_t j = 0; j < keypad[i].size(); ++j) {
+            if (keypad[i][j] == c) {
+                return std::make_pair(i, j);
+            }
+        }
+    }
+
+    return {};
+}
 
 std::optional<std::pair<size_t, size_t>> move(const std::vector<std::string> &keypad, std::pair<size_t, size_t> pos, char dir) {
     std::pair<size_t, size_t> old_pos = pos;
@@ -50,7 +63,30 @@ std::optional<std::pair<size_t, size_t>> move(const std::vector<std::string> &ke
     return {};
 }
 
-bool valid_sequence(const std::vector<std::string> &keypad, std::string sequence, std::pair<size_t, size_t> pos) {
+void gen_dirs(const std::vector<std::string> &keypad, std::string &sequence, std::string &dirs) {
+    std::pair<ssize_t, ssize_t> start_pos = get_pos(keypad, sequence[0]).value();
+    std::pair<ssize_t, ssize_t> end_pos = get_pos(keypad, sequence[sequence.size() - 1]).value();
+
+    std::pair<ssize_t, ssize_t> diff = std::make_pair(end_pos.first - start_pos.first, end_pos.second - start_pos.second);
+
+    char c = '<';
+    if (diff.first > 0) {
+        c = '>'; 
+    }
+    for (size_t i = 0; i < std::abs(diff.first); ++i) {
+        dirs += c;
+    }
+
+    c = '^';
+    if (diff.second > 0) {
+        c = 'v';
+    }
+    for (size_t i = 0; i < std::abs(diff.second); ++i) {
+        dirs += c;
+    }
+}
+
+bool valid_sequence(const std::vector<std::string> &keypad, std::string &sequence, std::pair<size_t, size_t> pos) {
     for (char c : sequence) {
         std::optional<std::pair<size_t, size_t>> potential_pos = move(keypad, pos, c);    
         if (!potential_pos.has_value()) {
@@ -67,11 +103,28 @@ bool valid_sequence(const std::vector<std::string> &keypad, std::string sequence
 }
 
 void gen_subseqs(
-    const std::vector<std::string> &keypad, 
-    std::pair<size_t, size_t> from, 
-    std::pair<size_t, size_t> to,
-    std::vector<std::string> &subseqs) {
+    unsigned int k,
+    std::string &subseq,
+    std::unordered_set<std::string> &unique_subseqs,
+    std::unordered_set<std::string> &valid_subseqs) {
+    if (k == 1) {
+        if (!unique_subseqs.contains(subseq)) {
+            unique_subseqs.emplace(subseq);
+            if (valid_sequence(dir_keypad, subseq, get_pos(dir_keypad, subseq[0]).value())) {
+                valid_subseqs.emplace(subseq);
+            }
+        }
+        return;
+    }
 
+    for (unsigned int i = 0; i < k - 1; ++i) {
+        if (k % 2 == 0) {
+            std::swap(subseq[i], subseq[k - 1]); 
+        } else {
+            std::swap(subseq[0], subseq[k - 1]);
+        }
+        gen_subseqs(k - 1, subseq, unique_subseqs, valid_subseqs);
+    }
 }
 
 std::string shortest_sequence(
@@ -84,19 +137,9 @@ void q21(std::ifstream &input_file) {
     std::string line;
     unsigned int total = 0;
     while (std::getline(input_file, line)) {
-        std::vector<char> dirs;
-
-        
-
-        for (char c : dirs) {
-            std::cout << c; 
-        }
-        std::cout << std::endl;
-        std::cout << dirs.size() << std::endl;
-
-        unsigned int complexity = dirs.size() * std::stoi(line.substr(0, line.size() - 1));
-        std::cout << complexity << std::endl;
-        total += complexity;
+        std::unordered_set<std::string> unique_subseqs;
+        std::unordered_set<std::string> valid_subseqs;
+        gen_subseqs(line.size(), line, unique_subseqs, valid_subseqs);
     }
     std::cout << total << std::endl;
 }
