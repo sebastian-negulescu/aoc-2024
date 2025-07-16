edd2bcad78f9b5d0e4afb3f3dddd2dab88e0f04b
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2025-07-15 22:17:10 -0400

    confused

--- q24.cpp
+++ q24.cpp
@@ -1,10 +1,13 @@
 #include "questions.h"
 
+#include <cstdlib>
 #include <assert.h>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
+#include <bitset>
+#include <format>
 #include <unordered_map>
 
 typedef struct expression_t {
@@ -38,11 +41,21 @@ bool solve(
     return values[reg];
 }
 
+void load_values(
+    std::unordered_map<std::string, bool> &values, 
+    std::string name, 
+    unsigned long long num, 
+    size_t bits
+) {
+    for (size_t bit = 0; bit < bits; ++bit) {
+        bool val = num & 0b1;
+        values[name + std::format("{:02}", bit)] = val;
+        num = num >> 1;
+    }
+}
+
 void q24(std::ifstream &input_file) {
     std::string line;
-
-    std::unordered_map<std::string, bool> values;
-    std::unordered_map<std::string, expression> expressions;
 
     while (std::getline(input_file, line)) {
         if (line == "") {
@@ -52,8 +65,10 @@ void q24(std::ifstream &input_file) {
         std::string reg_name = line.substr(0, 3);
         bool reg_value = std::stoi(line.substr(5, 1)) == 1;
 
-        values[reg_name] = reg_value;
+        // values[reg_name] = reg_value;
     }
+
+    std::unordered_map<std::string, expression> expressions;
 
     std::vector<std::string> z_regs;
     while (std::getline(input_file, line)) {
@@ -78,12 +93,30 @@ void q24(std::ifstream &input_file) {
         expressions[reg] = e;
     }
 
-    unsigned long long total = 0;
-    for (auto &z_reg : z_regs) {
-        bool value = solve(expressions, values, z_reg);
-        unsigned int position = std::stoi(z_reg.substr(1, z_reg.size() - 1));
-        total += (unsigned long long) value << position;
+    for (size_t i = 0; i < 100; ++i) {
+        std::unordered_map<std::string, bool> values;
+
+        unsigned long long x = 0;
+        unsigned long long y = 0;
+        size_t bits = 45;
+        for (size_t bit = 0; bit < bits; ++bit) {
+            x = (x << 1) | std::rand() % 2;
+            y = (y << 1) | std::rand() % 2;
+        }
+
+        unsigned int z = x + y;
+
+        load_values(values, "x", x, bits);
+        load_values(values, "y", y, bits);
+
+        unsigned long long total = 0;
+        for (auto &z_reg : z_regs) {
+            bool value = solve(expressions, values, z_reg);
+            unsigned int position = std::stoi(z_reg.substr(1, z_reg.size() - 1));
+            total += (unsigned long long) value << position;
+        }
+        std::cout << total << std::endl;
+        std::cout << std::bitset<46>(total ^ z) << std::endl;
     }
-    std::cout << total << std::endl;
 }
 
